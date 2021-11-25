7349ae2c270b430ec26ae7934fabdb17e69e0eea
Vlad Sovetov <vlad.sovetov@gmail.com>
2021-11-25 17:19:05 +0200

    style: Fixed too long line

--- src/components/SimpleButton/SimpleButton.tsx
+++ src/components/SimpleButton/SimpleButton.tsx
@@ -1,3 +1,4 @@
 import { ButtonHTMLAttributes, FC } from 'react'
 
-export const SimpleButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) => <button data-testid="simple-button" style={{ color: 'red' }} {...props} />
+export const SimpleButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> =
+  (props) => <button data-testid="simple-button" style={{ color: 'red' }} {...props} />
